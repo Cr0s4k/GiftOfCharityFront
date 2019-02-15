98dc04b0a37122cc709ef4351cf2c472a240b181
Omar Garcia <omarcruz11@hotmail.com>
2019-02-15 09:22:33 +0100

    Solved loading page troubles

--- src/containers/Item/index.js
+++ src/containers/Item/index.js
@@ -9,7 +9,7 @@ class Item extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            item: {},
+            item: undefined,
             error: false
         }
     }
@@ -17,7 +17,8 @@ class Item extends React.Component{
     componentDidMount() {
         API.getCharityProject(this.props.match.params.id)
             .then(item => {
-                this.setState({ item: item })
+                this.setState({ item: item });
+                document.title = item.name
             })
             .catch(e => {
                 this.setState({error: true})
@@ -28,23 +29,29 @@ class Item extends React.Component{
         document.location.href = "/"
     };
 
-    render() {
-        document.title = this.state.item !== undefined ? this.state.item.name : "";
+    handleError() {
+        document.title = "Error"
+        return (
+            <ErrorMessage
+                title="Error!"
+                description="The charity project you are trying to find it doesn't exists!"
+                handleClose={this.handleCloseError}
+            />
+        )
+    };
+
+    render(){
         return(
             <div className="divForFooter">
                 <TitleBar/>
-                {!this.state.error ?  (
+                { this.state.item && !this.state.error ?(
                     <SpecificItem item={this.state.item}/>
-                ) : (
-                    <ErrorMessage
-                        title="Error!"
-                        description="The charity project you are trying to find it doesn't exists!"
-                        handleClose={this.handleCloseError}
-                    />
-                )}
+                ): !this.state.item && !this.state.error ? (
+                    <div/>
+                ): this.handleError()}
                 <Footer/>
             </div>
-        )
+        );
     }
 }
 
