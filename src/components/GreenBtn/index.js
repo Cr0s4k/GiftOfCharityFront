717558fe37424c417ae895329534630780effb47
Omar Garcia <omarcruz11@hotmail.com>
2019-03-05 12:46:03 +0100

    Refactoring classes

--- src/components/GreenBtn/index.js
+++ src/components/GreenBtn/index.js
@@ -1,25 +1,11 @@
-import React from 'react'
 import Button from "@material-ui/core/Button";
 import withStyles from "@material-ui/core/es/styles/withStyles";
 
-const styles = {
-    greenBtn: {
+export default withStyles({
+    root: {
         backgroundColor: '#3c8e3f',
         '&:hover': {
             backgroundColor: '#0e6e10',
-        },
+        }
     }
-};
-
-class GreenBtn extends React.Component{
-    render() {
-        return (
-            <Button
-                {...this.props}
-                classes={{root: this.props.classes.greenBtn}}
-            />
-        );
-    }
-}
-
-export default withStyles(styles)(GreenBtn);
+})(Button);
--- src/components/GreenLinearProgress/index.js
+++ src/components/GreenLinearProgress/index.js
@@ -1,32 +1,14 @@
-import React from 'react'
 import {LinearProgress} from "@material-ui/core";
 import withStyles from "@material-ui/core/es/styles/withStyles";
 
-const styles = {
-    linearColorPrimary: {
+export default withStyles({
+    colorPrimary: {
         backgroundColor: "#ccffcd"
     },
-    linearBarColorPrimary: {
+    barColorPrimary: {
         backgroundColor: "#0e6e10"
     },
     root: {
         height: 7
     }
-};
-
-class GreenLinearProgress extends React.Component{
-    render() {
-        return (
-            <LinearProgress
-                {...this.props}
-                classes={{
-                    root: this.props.classes.root,
-                    colorPrimary: this.props.classes.linearColorPrimary,
-                    barColorPrimary: this.props.classes.linearBarColorPrimary
-                }}
-            />
-        );
-    }
-}
-
-export default withStyles(styles)(GreenLinearProgress);
+})(LinearProgress);
